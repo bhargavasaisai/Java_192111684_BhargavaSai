3e1b81deb105e065e19f5c5788198215a5b2ce7f
Bhargava Sai <114380524+bhargavasaisai@users.noreply.github.com>
2023-10-30 21:05:57 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,43 +1,68 @@
-class Bank 
+import java.util.Scanner;
+class StudentRecord 
 {
-    double getRateOfInterest() 
+    private String name;
+    private int registrationNumber;
+    private int[] marks = new int[5];
+    private int totalMarks;
+    private double averageMarks;
+    void setName(String studentName)
+     {
+        name = studentName;
+    }
+    void setRegistrationNumber(int regNumber) 
     {
-        return 0.0;
+        registrationNumber = regNumber;
+    }
+    void setMarks(int[] subjectMarks) 
+    {
+        marks = subjectMarks;
+    }
+    void calculateTotalMarks() 
+    {
+        for (int mark : marks) {
+            totalMarks += mark;
+        }
+    }
+
+    void calculateAverageMarks() 
+    {
+        averageMarks = (double) totalMarks / marks.length;
+    }
+
+    public void displayStudentInfo() 
+    {
+        System.out.println("Student Name: " + name);
+        System.out.println("Registration Number: " + registrationNumber);
+        System.out.println("Subject Marks: ");
+        for (int i = 0; i < marks.length; i++) 
+        {
+            System.out.println("Subject " + (i + 1) + ": " + marks[i]);
+        }
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Marks: " + averageMarks);
     }
 }
-class SBI extends Bank 
-{
-    @Override
-    double getRateOfInterest() 
-    {
-        return 8.4;
-    }
-}
-class ICICI extends Bank 
-{
-    @Override
-    double getRateOfInterest() 
-    {
-        return 7.3;
-    }
-}
-class AXIS extends Bank 
-{
-    @Override
-    double getRateOfInterest() 
-    {
-        return 9.7;
-    }
-}
-public class Main 
+class Main 
 {
     public static void main(String[] args) 
     {
-        SBI sbi = new SBI();
-        ICICI icici = new ICICI();
-        AXIS axis = new AXIS();
-        System.out.println("Rate of interest in SBI: " + sbi.getRateOfInterest() + "%");
-        System.out.println("Rate of interest in ICICI: " + icici.getRateOfInterest() + "%");
-        System.out.println("Rate of interest in AXIS: " + axis.getRateOfInterest() + "%");
+        Scanner scanner = new Scanner(System.in);
+        StudentRecord student = new StudentRecord();
+        System.out.print("Enter student name: ");
+        student.setName(scanner.nextLine());
+        System.out.print("Enter registration number: ");
+        student.setRegistrationNumber(scanner.nextInt());
+        int[] marks = new int[5];
+        System.out.println("Enter subject marks:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+        }
+        student.setMarks(marks);
+        student.calculateTotalMarks();
+        student.calculateAverageMarks();
+        student.displayStudentInfo();
+        scanner.close();
     }
 }
